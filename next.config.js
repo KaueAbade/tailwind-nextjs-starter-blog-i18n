d7d8f6ecb4aeb484938b538772ebb6a50810ea76
Kauê Abade <kaue.abade@outlook.com>
2026-08-04 12:52:28 -0300

    feat: reescrevendo função de conversão de SVG so webpack como turbopack

--- next.config.js
+++ next.config.js
@@ -77,6 +77,14 @@ module.exports = () => {
         },
       ]
     },
+    turbopack: {
+      rules: {
+        '*.svg': {
+          loaders: ['@svgr/webpack'],
+          as: '*.js',
+        },
+      },
+    },
     webpack: (config, options) => {
       config.module.rules.push({
         test: /\.svg$/,
